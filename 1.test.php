df4194835c167f4900a1cef332324f75bc0d53cb
brian8234214 <63779101+brian8234214@users.noreply.github.com>
2020-11-06 14:36:46 +0800

    Update 1.test.php

--- 1.test.php
+++ 1.test.php
@@ -1,5 +1,5 @@
 <?php 
-phpinfo(); <!--PHP info 函數是 PHP 設計師用來了解系統上PHP大量資訊的重要函數，舉凡 PHP 的版本（PHP version）......都可以用 phpinfo 函數來檢查。-->
+phpinfo(); //PHP info 函數是 PHP 設計師用來了解系統上PHP大量資訊的重要函數，舉凡 PHP 的版本（PHP version）......都可以用 phpinfo 函數來檢查。
 ?>
 
-<!--一般的情況下，第一種寫法就能顯示出 PHP 資訊-->
+//一般的情況下，第一種寫法就能顯示出 PHP 資訊
